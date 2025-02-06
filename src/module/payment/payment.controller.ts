18194056c136d0b27faa0a81b25e638ee140bf3d
aminvv <aminviper1378@gmail.com>
2025-02-06 12:46:32 +0330

    verify gateway payment----------->>>>>>>>>>(chapter:11 section:25)

--- src/module/payment/payment.controller.ts
+++ src/module/payment/payment.controller.ts
@@ -1,20 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Res } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { swaggerConsumes } from 'src/common/enum/swaggerConsumes.enum';
 import { ApiConsumes, ApiProperty } from '@nestjs/swagger';
-export class addressDto{
+import { Response } from 'express';
+export class addressDto {
   @ApiProperty()
-  address:string
+  address: string
 }
 
 @Controller('payment')
 export class PaymentController {
-  constructor(private readonly paymentService: PaymentService) {}
+  constructor(private readonly paymentService: PaymentService) { }
 
   @Post()
- @ApiConsumes(swaggerConsumes.UrlEncoded)
-   create(@Body() addressDto:addressDto ) {
+  @ApiConsumes(swaggerConsumes.UrlEncoded)
+  create(@Body() addressDto: addressDto) {
     return this.paymentService.create(addressDto.address);
+  }
+  @Get("/verify")
+  @ApiConsumes(swaggerConsumes.UrlEncoded)
+  async verify(@Query("authority") authority: string, @Query("status") status: string, @Res() res: Response) {
+    const url = await this.paymentService.verify(authority, status);
+    return res.redirect(url)
+  }
+  @Get("/find-payment")
+  async find() {
+    return this.paymentService.find();
+
   }
 
 
--- src/module/payment/payment.service.ts
+++ src/module/payment/payment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, Redirect } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaymentEntity } from './entities/payment.entity';
 import { Repository } from 'typeorm';
@@ -13,14 +13,14 @@ import { addressDto } from './payment.controller';
 @Injectable()
 export class PaymentService {
   constructor(
-    @InjectRepository(PaymentEntity)private paymentRepository:Repository<PaymentEntity>,
-    @InjectRepository(OrderEntity)private orderRepository:Repository<OrderEntity>,
-    @InjectRepository(OrderItemEntity)private orderItemRepository:Repository<OrderItemEntity>,
-    private basketService:BasketService,
-    private zarinnpalService:ZarinnpalService,
-  ){}
+    @InjectRepository(PaymentEntity) private paymentRepository: Repository<PaymentEntity>,
+    @InjectRepository(OrderEntity) private orderRepository: Repository<OrderEntity>,
+    @InjectRepository(OrderItemEntity) private orderItemRepository: Repository<OrderItemEntity>,
+    private basketService: BasketService,
+    private zarinnpalService: ZarinnpalService,
+  ) { }
   async create(address: string) {
-    const user = {email: "erfanyousefi.co@gmail.com", mobile: "09332255768"};
+    const user = { email: "erfanyousefi.co@gmail.com", mobile: "09332255768" };
     const basket = await this.basketService.getBasket();
     let order = this.orderRepository.create({
       final_amount: basket.finalAmount,
@@ -40,7 +40,7 @@ export class PaymentService {
       };
     });
     await this.orderItemRepository.insert(orderItems);
-    const {authority, gateWayUrl} = await this.zarinnpalService.sendRequest({
+    const { authority, gateWayUrl } = await this.zarinnpalService.sendRequest({
       amount: basket.finalAmount,
       description: "خرید محصولات فیزیکی",
       user,
@@ -55,6 +55,33 @@ export class PaymentService {
     payment = await this.paymentRepository.save(payment);
     order.paymentId = payment.id;
     await this.orderRepository.save(order);
-    return {gateWayUrl};
+    return { gateWayUrl };
+  }
+
+  async verify(authority: string, status: string) {
+    const payment = await this.paymentRepository.findOneBy({ authority })
+    if (!payment) throw new NotFoundException("not found payment")
+    if (payment.status) throw new BadRequestException("already verified payment")
+    if (status === "OK") {
+      const order = await this.orderRepository.findOneBy({ id: payment.id })
+      if (!order) throw new NotFoundException("orderNotFound")
+      order.status = OrderStatus.Ordered
+      payment.status = true
+      await Promise.all([
+        this.paymentRepository.save(payment),
+        this.orderRepository.save(order)
+      ])
+      return "http://frontEndUrl/payment/success? order_no="+ order.id
+    } else {
+      return "http://frontEndUrl/payment/failedUrl"
+    }
+  }
+
+  async find(){
+    return this.paymentRepository.find({
+      order:{
+        create_at:"DESC"
+      }
+    })
   }
 }
